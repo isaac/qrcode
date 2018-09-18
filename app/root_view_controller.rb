b8979b4e1c4b8f5821fa68ffd09c7afc6f874ae1
Isaac Kearse <isaac@kearse.co.nz>
2018-09-18 17:34:59 +1200

    Hack something together that works

--- app/root_view_controller.rb
+++ app/root_view_controller.rb
@@ -1,16 +1,36 @@
 class RootViewController < UIViewController
+  attr_accessor :output, :capture, :input
+
   def viewDidLoad
-    video = AVMediaTypeVideo
-    AVCaptureDevice.requestAccessForMediaType(video, completionHandler:lambda do |granted|
-      capture = AVCaptureSession.new
-      self.view = PreviewView.new
-      view.layer.session = capture
-      device = AVCaptureDevice.defaultDeviceWithMediaType video
-      input = AVCaptureDeviceInput.deviceInputWithDevice device, error:nil
-      output = AVCaptureMetadataOutput.new
-      capture.addInput input
-      capture.addOutput output
-      capture.startRunning
-    end)
+    AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler:lambda { |granted|completionHandler granted })
+  end
+
+  def completionHandler granted
+    self.capture = AVCaptureSession.new
+    capture.beginConfiguration
+    self.view = PreviewView.new
+    view.layer.session = capture
+    device = AVCaptureDevice.defaultDeviceWithMediaType AVMediaTypeVideo
+    self.input = AVCaptureDeviceInput.deviceInputWithDevice device, error:nil
+    self.output = AVCaptureMetadataOutput.new
+    output.setMetadataObjectsDelegate self, queue:Dispatch::Queue.main.dispatch_object
+    capture.addInput input
+    capture.addOutput output
+    output.metadataObjectTypes = [ AVMetadataObjectTypeQRCode ]
+    capture.commitConfiguration
+    capture.startRunning
+  rescue => exception
+    puts "Rescued exception"
+    puts exception
+    puts exception.name
+    puts exception.reason
+    puts exception.userInfo
+  end
+
+  def captureOutput output, didOutputMetadataObjects:objects, fromConnection:connection
+    puts "didOutputMetadataObjects: #{ objects }"
+    objects.each do |object|
+      
+    end
   end
 end
